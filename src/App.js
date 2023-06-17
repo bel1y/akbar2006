f26cdd17adf8f7331d0b44364e539c9c04b4463c
storm77777777777777 <86982819+abbassiriyus@users.noreply.github.com>
2023-06-17 11:55:54 +0500

    jkdsf
    sdf

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import calvin from './soliyev.png'
+function App() {
+  return (
+    <div >
+      <header>
+        <div className="soliyev">
+<div className="akbar">
+<div className="seokjin"><img src={calvin} alt="" />
+<select className='callin' id="">
+  <option className='callin' value="">Barcha kurslar</option>
+  <option className='callin' value="">Barcha kurslar</option>
+  <option className='callin' value="">Barcha kurslar</option>
+  <option className='callin' value="">Barcha kurslar</option>
+  <option className='callin' value="">Barcha kurslar</option>
+</select>
+</div>
+<div className="seokjin2">
+<select className='mic' id="">
+  <option className='mic' value="">UZBEK</option>
+  <option className='mic' value="">UZBEK</option>
+  <option className='mic' value="">UZBEK</option>
+  <option className='mic' value="">UZBEK</option>
+  <option className='mic' value="">UZBEK</option>
+
+</select>
+<button className="ibo">KIRISH</button>
+</div>
+</div>
+
+
+</div>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
