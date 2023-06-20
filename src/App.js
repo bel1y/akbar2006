17bac9c3cc066531c50121039735055c15abcef1
storm77777777777777 <131956801+fot1ma@users.noreply.github.com>
2023-06-20 11:02:41 +0500

    v7yt7b

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import calvin from './soliyev.png'
+import calvin from './soliyev.png';
 function App() {
   return (
     <div >
@@ -9,25 +9,24 @@ function App() {
 <div className="seokjin"><img src={calvin} alt="" />
 <select className='callin' id="">
   <option className='callin' value="">Barcha kurslar</option>
-  <option className='callin' value="">Barcha kurslar</option>
-  <option className='callin' value="">Barcha kurslar</option>
-  <option className='callin' value="">Barcha kurslar</option>
-  <option className='callin' value="">Barcha kurslar</option>
+  <option className='callin' value="">1-kurs</option>
+  <option className='callin' value="">2-kurs</option>
+  <option className='callin' value="">3-kurs</option>
+  <option className='callin' value="">4-kurs</option>
 </select>
 </div>
 <div className="seokjin2">
 <select className='mic' id="">
   <option className='mic' value="">UZBEK</option>
-  <option className='mic' value="">UZBEK</option>
-  <option className='mic' value="">UZBEK</option>
-  <option className='mic' value="">UZBEK</option>
-  <option className='mic' value="">UZBEK</option>
-
+  <option className='mic' value="">RUS</option>
+  <option className='mic' value="">ENGLISH</option>
 </select>
 <button className="ibo">KIRISH</button>
 </div>
 </div>
+<div className="swan">
 
+</div>
 
 </div>
       </header>
---
+++ src/pages/js/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import NavbarMe from './NavbarMe'
+import FooterMe from './FooterMe' 
+import s '../css/Home.module.css'
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <NavbarMe/>
+<header>
+
+
+
+
+
+
+
+</header>
+
+        <FooterMe/>
+      </div>
+    )
+  }
+}
