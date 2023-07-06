a5254e6778e38c2801b1de3e333935c819805e1d
storm77777777777777 <131956801+fot1ma@users.noreply.github.com>
2023-07-06 13:36:50 +0500

    rtrt
    rtrtdrt

--- src/pages/js/Home.js
+++ src/pages/js/Home.js
@@ -7,17 +7,53 @@ import { AiFillStar } from "react-icons/ai";
 import { AiOutlineStar } from "react-icons/ai";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import blabla from "./guh.png";
-import blabla1 from "./hozr.png"
+import blabla1 from "./hozr.png";
+import blan from "./iibo.png";
 export default class Home extends Component {
   state={
     data:[],
-    t:localStorage.getItem("lang")?localStorage.getItem("lang"):'uz'
+    t:localStorage.getItem("lang")?localStorage.getItem("lang"):'uz',
+    teacher:[{
+    teacherid:1,
+    teacherName_uz:"Abbas_uz",
+    teacherName_ru:"Abbas_ru"
+  },
+{
+  teacherid:2,
+  teacherName_uz:"Abbs_uz",
+  teacherName_ru:"Abbs_ru"
+}],
+  t:"uz"
+}
+postData=()=>{
+  var data={
+    'course_teacherid':document.querySelector("#mm").value*1,
+    'course_title_ru':document.querySelector("#jk").value,
+    "course_img":document.querySelector("#th").files[0],
+    "course_price":document.querySelector("#hu").value,
+    "course_time":document.querySelector("#mk").value,
+    "course_teacherid": 5,
+   }
+   console.log(data);
   }
-
   render() {
     return (
       <div>
         <NavbarMe/>
+
+
+        <input id='jk' type="text" />
+        <select name="" id="mm">
+          {this.state.teacher.map(item=>{
+            return <option value={item.teacherid}>{this.state.t==="uz"?(item.teacherName_uz):(this.state.teacherName_ru)}</option>
+          })}
+        </select>
+        <input type="file" id='th' />
+        <input type="number" id='hu' />
+        <input type="number" id='mk' />
+        <button onClick={()=>{this.postData()}} >Yuborish</button>
+
+
 <header className={s.bosh_menu} >
   <div className={s.aylanaab} >    
   </div>
@@ -95,15 +131,8 @@ e’tibor </p>
     </div>
 </div>
 <div className={s.card2} ></div>
-
-
-
-
-
 </div>
-
 </header>
-
         <FooterMe/>
         <div className={s.cas}>
 <center><p className={s.min} >Bizning eng top kurslarimizni ko'rib chiqing</p></center>
@@ -132,6 +161,25 @@ kurs. (amaliy)</p>
   <img src={ blabla1 } alt="" />
 <div className={s.hugug1}>  <p className={s.lala1} >Illustrator boshlang’ichdan <br />
 proffessionalgacha</p>
+<div className={s.icon} >
+     <h3> <AiFillStar /></h3>
+    <h3> <AiFillStar /></h3>
+    <h3> <AiFillStar /></h3>
+    <h3> <AiFillStar /></h3>
+    <h3><AiOutlineStar/></h3> <p>4.1</p>
+    <p  className={s.brrrr} >(524)</p>
+    <div className={s.bt}></div>
+    </div></div>
+<div className={s.okeoke}>
+<div className={s.oke1}> <p className={s.bhbh}>Kurs hajmi</p> <p className={s.bhbh1} >38 soat</p> </div>
+<div className={s.oke2}><p className={s.bhbh}>Kurs narxi</p> <p className={s.bhbh1} >250.000 <span>UZS</span> </p></div>
+<div className={s.oke2}><button className={s.ibf3} ><h3><AiOutlineArrowRight/></h3></button></div>
+</div>
+</div>
+
+<div className={s.nun3}>
+  <img src={ blan } alt="" />
+<div className={s.hugug1}>  <p className={s.lala1} >Boshlang’ich Photoshop <br />kurslari</p>
 <div className={s.icon} >
      <h3> <AiFillStar /></h3>
     <h3> <AiFillStar /></h3>
